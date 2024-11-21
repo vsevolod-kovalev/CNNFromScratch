73a5ec2b4c2974ff5b8fcb09f5316f1efc36aadf
vsevolod-kovalev <skillkolen@gmail.com>
2024-11-21 13:59:55 -0800

    new: added reLU activation support for Conv2D

--- Layer.py
+++ Layer.py
@@ -2,13 +2,25 @@ import random
 
 RANDOM_WEIGHT_RANGE = 0.1
 
+# assuming l has even row and column distibution
+def shape(l):
+    shape = []
+    while True:
+        if isinstance(l, list):
+            shape.append(len(l))
+            l = l[0]
+        else:
+            break
+    return shape
+
 class Layer:
     def __init__(self):
         self.input = None
+        self.preactivation = None
         self.output = None
 
 class Conv2D(Layer):
-    def __init__(self, filters: int, kernel_size: int, strides: int, padding: str):
+    def __init__(self, filters: int, kernel_size: int, strides: int, padding: str, activation_function: str):
         super().__init__()
         self.weights = []
         self.biases = [random.uniform(-RANDOM_WEIGHT_RANGE, RANDOM_WEIGHT_RANGE) for _ in range(filters)]
@@ -18,6 +30,31 @@ class Conv2D(Layer):
         if not (padding == 'same' or padding == 'valid'):
             raise Exception("Unknown padding format. Conv2D padding can be \'valid\' or \'same\'.")
         self.padding = padding
+        self.activation_function = activation_function
+
+    # Activation functions
+    @staticmethod
+    def reLU(layer, derivative: bool = False):
+        depth, height, width = len(layer), len(layer[0]), len(layer[0][0])
+        output  = [
+            [[0.0 for _ in range(width)]
+                  for __ in range(height)]
+                  for ___ in range(depth)
+        ]
+        relu = lambda z: (1.0 if z > 0 else 0.0) if derivative else (max(0, z))
+        for i in range(depth):
+            for j in range(height):
+                for k in range(width):
+                    z = layer[i][j][k]
+                    output[i][j][k] = relu(z)
+        return output
+    @staticmethod
+    def activate(layer, activation: str):
+        match activation.lower():
+            case 'relu':
+                return Conv2D.reLU(layer)
+            case _:
+                raise Exception("Unknown activation function")
     @staticmethod
     def add_padding(input, padding):
         depth, height, width = len(input), len(input[0]), len(input[0][0])
@@ -49,10 +86,10 @@ class Conv2D(Layer):
         if self.padding == 'same':
             # calculate the neccesary padding to maintain the input size:
             padding = (self.strides * width - self.strides - width + self.kernel_size) / 2
-        if padding % 1 != 0 or padding < 1:
-            raise Exception("Padding must be a positive integer.")
+        if padding % 1 != 0 or padding < 0:
+            raise Exception("Padding must be a non-negative integer.")
         padding = int(padding)
-        output = []
+        Z = []
         if width != height:
             raise Exception("Input width must be the same as input height.")
         feature_map_size = (width + padding * 2 - self.kernel_size) / self.strides + 1
@@ -61,6 +98,7 @@ class Conv2D(Layer):
         feature_map_size = int(feature_map_size)
         if padding:
             input = Conv2D.add_padding(input, padding)
+  
         for filter_index in range(self.num_filters):
             feature_map = [
                 [0.0 for _ in range(feature_map_size)] for __ in range(feature_map_size)
@@ -80,9 +118,10 @@ class Conv2D(Layer):
                     # Add bias
                     filter_bias = self.biases[filter_index]
                     feature_map[starting_i][starting_j] = _sum + filter_bias
-            output.append(feature_map)
-        self.output = output
-        return output
+            Z.append(feature_map)
+        self.preactivation = Z
+        self.output = Conv2D.activate(Z, self.activation_function)
+        return self.output
 class AveragePooling2D(Layer):
     def __init__(self, pool_size: int, strides: int):
         super().__init__()
@@ -115,15 +154,15 @@ class AveragePooling2D(Layer):
                     feature_map[output_i][output_j] = _sum / pool_area
             output.append(feature_map)
         self.output = output
-        print(self.output)
         return output
 
-c = Conv2D(filters = 6, kernel_size = 3, strides = 1, padding = 'same')
+c = Conv2D(filters = 6, kernel_size = 3, strides = 1, padding = 'same', activation_function = 'relu')
 step_1 = c.forward(
     [[[1.0 for _ in range(4)] for _ in range(4)] for _ in range(3)]
 )
 b = AveragePooling2D(pool_size = 2, strides = 2)
 step_2 = b.forward(step_1)
+print(step_2, shape(step_2))
 
 
                     
