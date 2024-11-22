ef85f73954a825d2aae1e89d06c8b9e49fae1aa3
vsevolod-kovalev <skillkolen@gmail.com>
2024-11-21 18:31:59 -0800

    new: added Dense class to support fully connected layers

--- Layer.py
+++ Layer.py
@@ -7,6 +7,13 @@ class Layer:
         self.input = None
         self.preactivation = None
         self.output = None
+    
+    def activate(self, Z, activation: str):
+        match activation.lower():
+            case 'relu':
+                return self.reLU(Z)
+            case _:
+                raise Exception("Unknown activation function")
     # assuming l has even row and column distibution
     @staticmethod
     def shape(_list):
@@ -33,9 +40,41 @@ class Flatten(Layer):
         access(input)
         self.output = output
         return self.output
-
+class Dense(Layer):
+    def __init__(self, num_neurons: int, activation: str):
+        self.num_neurons = num_neurons
+        self.weights = []
+        self.biases = [random.uniform(-RANDOM_WEIGHT_RANGE, RANDOM_WEIGHT_RANGE) for _ in range(num_neurons)]
+        self.activation_function = activation
+    def reLU(self, Z, derivative: bool = False):
+        output = []
+        relu = lambda z: (1.0 if z > 0 else 0.0) if derivative else (max(0, z))
+        for z in Z:
+            output.append(relu(z))
+        return output
+    def forward(self, input):
+        Z = []
+        input_size = len(input)
+        if not self.weights:
+            self.weights = [[random.uniform(-RANDOM_WEIGHT_RANGE, RANDOM_WEIGHT_RANGE)
+                             for _ in range(input_size)]
+                             for __ in range(self.num_neurons)]
+        # compute neurons pre-activations:
+        for neuron_index in range(self.num_neurons):
+            # number of weights in each neuron is equal
+            # to the size of the flattened input:
+            z = sum([
+                input[input_index] * self.weights[neuron_index][input_index]
+                for input_index in range(input_size)
+            ]) + self.biases[neuron_index]
+            Z.append(z)
+        # apply specified activation function
+        self.preactivation = Z
+        self.output = self.activate(Z, self.activation_function)
+        return self.output
+            
 class Conv2D(Layer):
-    def __init__(self, filters: int, kernel_size: int, strides: int, padding: str, activation_function: str):
+    def __init__(self, filters: int, kernel_size: int, strides: int, padding: str, activation: str):
         super().__init__()
         self.weights = []
         self.biases = [random.uniform(-RANDOM_WEIGHT_RANGE, RANDOM_WEIGHT_RANGE) for _ in range(filters)]
@@ -45,31 +84,8 @@ class Conv2D(Layer):
         if not (padding == 'same' or padding == 'valid'):
             raise Exception("Unknown padding format. Conv2D padding can be \'valid\' or \'same\'.")
         self.padding = padding
-        self.activation_function = activation_function
+        self.activation_function = activation
 
-    # Activation functions
-    @staticmethod
-    def reLU(layer, derivative: bool = False):
-        depth, height, width = len(layer), len(layer[0]), len(layer[0][0])
-        output  = [
-            [[0.0 for _ in range(width)]
-                  for __ in range(height)]
-                  for ___ in range(depth)
-        ]
-        relu = lambda z: (1.0 if z > 0 else 0.0) if derivative else (max(0, z))
-        for i in range(depth):
-            for j in range(height):
-                for k in range(width):
-                    z = layer[i][j][k]
-                    output[i][j][k] = relu(z)
-        return output
-    @staticmethod
-    def activate(layer, activation: str):
-        match activation.lower():
-            case 'relu':
-                return Conv2D.reLU(layer)
-            case _:
-                raise Exception("Unknown activation function")
     @staticmethod
     def add_padding(input, padding):
         depth, height, width = len(input), len(input[0]), len(input[0][0])
@@ -83,6 +99,21 @@ class Conv2D(Layer):
                 for k in range(width):
                     padded_input[i][j + padding][k + padding] = input[i][j][k]
         return padded_input
+    def reLU(self, layer, derivative: bool = False):
+        depth, height, width = len(layer), len(layer[0]), len(layer[0][0])
+        output  = [
+            [[0.0 for _ in range(width)]
+                  for __ in range(height)]
+                  for ___ in range(depth)
+        ]
+        relu = lambda z: (1.0 if z > 0 else 0.0) if derivative else (max(0, z))
+        for i in range(depth):
+            for j in range(height):
+                for k in range(width):
+                    z = layer[i][j][k]
+                    output[i][j][k] = relu(z)
+        return output
+
     def forward(self, input):
         depth, height, width = len(input), len(input[0]), len(input[0][0])
         if not self.weights:
@@ -135,7 +166,7 @@ class Conv2D(Layer):
                     feature_map[starting_i][starting_j] = _sum + filter_bias
             Z.append(feature_map)
         self.preactivation = Z
-        self.output = Conv2D.activate(Z, self.activation_function)
+        self.output = self.activate(Z, self.activation_function)
         return self.output
 class AveragePooling2D(Layer):
     def __init__(self, pool_size: int, strides: int):
@@ -171,7 +202,10 @@ class AveragePooling2D(Layer):
         self.output = output
         return output
 
-c = Conv2D(filters = 6, kernel_size = 3, strides = 1, padding = 'same', activation_function = 'relu')
+
+
+
+c = Conv2D(filters = 6, kernel_size = 3, strides = 1, padding = 'same', activation = 'relu')
 step_1 = c.forward(
     [[[1.0 for _ in range(4)] for _ in range(4)] for _ in range(3)]
 )
@@ -181,6 +215,9 @@ print(step_2, Layer.shape(step_2))
 a = Flatten()
 step_3 = a.forward(step_2)
 print(step_3, Layer.shape(step_3))
+f = Dense(10, 'relu')
+step_4 = f.forward(step_3)
+print(step_4, Layer.shape(step_4))
 
 
                     
